bfc589f670b14a337bbed56762008899a0d2bb0b
David Medina <davidchristian.m@gmail.com>
2025-04-24 00:37:15 +0000

    solicitud de notas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-alert("Hola")
+alert("Hola calculemos tu promedio!")
+
+
+    let h1 = prompt('Ingrese nota 1 de HTML: ')
+    document.getElementById('HTML-N1').textContent = h1
+
+    let h2 = prompt('Ingrese nota 2 de HTML: ')
+    document.getElementById('HTML-N2').textContent = h2
+
+    let h3 = prompt('Ingrese nota 3 de HTML: ')
+    document.getElementById('HTML-N3').textContent = h3
+
+    let c1 = prompt('Ingrese nota 1 de CSS: ')
+    document.getElementById('CSS-N1').textContent = c1
+
+    let c2 = prompt('Ingrese nota 2 de CSS: ')
+    document.getElementById('CSS-N2').textContent = c2
+
+    let c3 = prompt('Ingrese nota 3 de CSS: ')
+    document.getElementById('CSS-N3').textContent = c3
+
+    let j1 = prompt('Ingrese nota 1 de JavaScript: ')
+    document.getElementById('JS-N1').textContent = j1
+
+    let j2 = prompt('Ingrese nota 2 de JavaScript: ')
+    document.getElementById('JS-N2').textContent = j2
+
+    let j3 = prompt('Ingrese nota 3 de JavaScript: ')
+    document.getElementById('JS-N3').textContent = j3
+    
+    
